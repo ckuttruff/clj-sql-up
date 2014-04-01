8d85b20be8c7d4e4229ecd11298e3eed98d58354
Peter Schwarz <peter.schwarz@bicycle.io>
2014-04-01 14:29:56 -0500

    Added test for migrating again

--- test/clj_sql_up/migration_test.clj
+++ test/clj_sql_up/migration_test.clj
@@ -9,7 +9,7 @@
               :subname "mem:testdb"})
 
 (defn- count-records [db table-name]
-  (-> 
+  (->
     (sql/query db-spec [(str "select count(*) from " table-name)])
     first
     :c1))
@@ -49,4 +49,21 @@
         (is (thrown? Exception (count-records db-spec "bbb")))
         (is (thrown? Exception (count-records db-spec "aaa")))))
 
+    (testing "migrate again"
+       (m/migrate db-spec)
+
+       (let [completed-migrations (m/completed-migrations db-spec)]
+         (is (= 4 (count completed-migrations)))
+         (is (= 0 (count-records db-spec "aaa")))
+         (is (= 0 (count-records db-spec "bbb")))
+         (is (= 0 (count-records db-spec "ccc")))
+         (is (= 0 (count-records db-spec "zzz")))))
+
+
   ))
+
+
+
+
+
+
